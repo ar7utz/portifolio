b4c735b032c2ca489b56b5c3bbd087e34a26f86f
ar7utz <pereira.artur@hotmail.com>
2023-05-18 16:32:14 -0300

    to entendendo mais nothing

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,25 +8,45 @@
     <link rel="stylesheet" href="css/style.css">
     <title>Portifólio</title>
 </head>
-<body>
-    <div id="corpo">
-        <header>
-            <div id="navtop">
-                <nav>
-                    <ul>
-                        <li><a href="">HOME</a></li>
-                        <li><a href="#sobre">SOBRE</a></li>
-                        <li><a href="">CONTATO</a></li>
-                    </ul>
-                </nav>
-            </div>   
-        </header>
 
-        <div id="sobre">
-            <h1>SOBRE</h1>
-                <p>Meu nome é Artur tenho 20 anos, faço Análise e Desenvolvimento de Sistemas na Fatec de Araçatuba</p>
-                <p>e estudo linguagens de aprofundamento back-end, dando ênfase em PHP, JavaScript e coligados.</p>
+<body>
+    <header>
+        <div id="navtop">
+            <nav>
+                <ul>
+                    <li><a href="#">HOME</a></li>
+                    <li><a href="#sobre">SOBRE</a></li>
+                    <li><a href="#contato">CONTATO</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+
+    <div id="sobre">
+        <h1>SOBRE</h1>
+        <p>Meu nome é Artur tenho 20 anos, faço Análise e Desenvolvimento de Sistemas na Fatec de Araçatuba</p>
+        <p>e estudo linguagens de aprofundamento back-end, dando ênfase em PHP, JavaScript e coligados.</p>
+    </div>
+
+    <div id="contato">
+        <h1>CONTATO</h1>
+        <p>email pessoal: pereira.artur@hotmail.com</p>
+        <p>email institucional: artur.pereira4@fatec.sp.gov.br</p>
+        <div id="form">
+            <form action="" method="post">
+                <h1>Me mande um email!</h1>
+                <label for="nome">Nome:</label>
+                <input type="text" name="nome">
+                <label for="email">Email:</label>
+                <input type="text" name="email">
+                <label for="celular">Celular:</label>
+                <input type="number" name="cel">
+                <label for="assunto">Assunto:</label>
+                <input type="text" name="assunto">
+            </form>
         </div>
     </div>
+
 </body>
+
 </html>
