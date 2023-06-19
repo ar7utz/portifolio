26817e557af0a0e75a30d8f365590876af70eb3f
Artur da Silva Pereira <90013149+ar7utz@users.noreply.github.com>
2023-06-18 22:50:07 -0300

    leves alterações no css

--- index.php
+++ index.php
@@ -3,48 +3,31 @@
 
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
     <title>Portifólio</title>
 </head>
 
 <body>
     <header>
-        <div id="navtop">
-            <nav>
-                <ul>
-                    <li><a href="#">HOME</a></li>
-                    <li><a href="#sobre">SOBRE</a></li>
-                    <li><a href="#contato">CONTATO</a></li>
-                </ul>
-            </nav>
-        </div>
+        <nav>
+            <ul>
+                <li><a href="#">HOME</a></li>
+                <li><a href="#sobre">SOBRE</a></li>
+                <li><a href="#contato">CONTATO</a></li>
+            </ul>
+        </nav>
     </header>
 
-    <div id="sobre">
+    <div class="iten1">
         <h1>SOBRE</h1>
         <p>Meu nome é Artur tenho 20 anos, faço Análise e Desenvolvimento de Sistemas na Fatec de Araçatuba</p>
-        <p>e estudo linguagens de aprofundamento back-end, dando ênfase em PHP, JavaScript e coligados.</p>
+        <p>e estudo linguagens de aprofundamento back-end como PHP, JavaScript, entre outros.</p>
     </div>
 
-    <div id="contato">
+    <div class="iten1">
         <h1>CONTATO</h1>
         <p>email pessoal: pereira.artur@hotmail.com</p>
         <p>email institucional: artur.pereira4@fatec.sp.gov.br</p>
-        <div id="form">
-            <form action="" method="post">
-                <h1>Me mande um email!</h1>
-                <label for="nome">Nome:</label>
-                <input type="text" name="nome">
-                <label for="email">Email:</label>
-                <input type="text" name="email">
-                <label for="celular">Celular:</label>
-                <input type="number" name="cel">
-                <label for="assunto">Assunto:</label>
-                <input type="text" name="assunto">
-            </form>
-        </div>
     </div>
 
 </body>
