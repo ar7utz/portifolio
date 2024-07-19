4d6facb16458557f67020bb5a6741866d8da3143
ar7utz <pereira.artur@hotmail.com>
2024-07-18 21:28:13 -0300

    add bootstrap

--- index.php
+++ index.php
@@ -1,35 +1,36 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
-
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="./assets/css/bootstrap.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/cssNativo/style.css">
     <title>Portifólio</title>
 </head>
-
 <body>
-    <header>
-        <nav>
-            <ul>
-                <li><a href="#">HOME</a></li>
-                <li><a href="#sobre">SOBRE</a></li>
-                <li><a href="#contato">CONTATO</a></li>
-            </ul>
-        </nav>
-    </header>
-
-    <div class="iten1">
-        <h1>SOBRE</h1>
-        <p>Meu nome é Artur tenho 20 anos, faço Análise e Desenvolvimento de Sistemas na Fatec de Araçatuba</p>
-        <p>e estudo linguagens de aprofundamento back-end como PHP, JavaScript, entre outros.</p>
+    <div class="content">
+        <div class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">teste</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">teste1</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">teste2</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
     </div>
-
-    <div class="iten1">
-        <h1>CONTATO</h1>
-        <p>email pessoal: pereira.artur@hotmail.com</p>
-        <p>email institucional: artur.pereira4@fatec.sp.gov.br</p>
-    </div>
-
+    <script src="./assets/js/bootstrap.js"></script>
 </body>
-
 </html>
